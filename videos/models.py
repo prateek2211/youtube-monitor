bdb8a6cd3eb8178e94f2b0c056ead4ae0e427e3e
Prateek Sachan <psachan@cs.iitr.ac.in>
2019-11-24 04:57:25 +0530

    Added task to delete few records every hour

--- videos/models.py
+++ videos/models.py
@@ -10,5 +10,6 @@ class Video(models.Model):
 
     def __str__(self):
         return self.title
+
     class Meta:
         ordering = ['-publish_time']
--- videos/tasks.py
+++ videos/tasks.py
@@ -1,5 +1,7 @@
 from celery.task import periodic_task
+from celery.schedules import crontab
 from videos.utils import Client
+from videos.models import Video
 
 c = Client()
 
@@ -8,3 +10,9 @@ c = Client()
 def fetch_job():
     print('Making API requests')
     c.cron_job()
+
+
+# delete last 20 records every hour
+@periodic_task(run_every=crontab(hour='*', minute=1))
+def clean_database():
+    Video.objects.reverse()[:20].delete()
